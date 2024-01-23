ac091c4cac36b1dc51d612b59fa0f3d41d81565a
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-01-23 22:38:18 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+from pypdf2 import pdf writer
+import os
+
+merger = pdfWriter()
+files = [file for file in os.listdir() if file.endSwitch(".pdf")
+for pdf in files:
+ merger.write("merged-pdf.pdf")
+ merger.close()
